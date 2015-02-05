535c97ff60a6beff9a7ef0a3bd3b6fc5f1e0c003
XX <freecoder.xx@gmail.com>
2015-02-06 00:42:44 +0400

    Added Users controller

--- app/main/controller/UsersController.php
+++ app/main/controller/UsersController.php
@@ -2,6 +2,16 @@
 
 class UsersController extends AbstractController {
     
+    public function indexAction() {
+        $this->response->body('<h2>executed Users.index</h2>');
+    }
     
+    public function editAction() {
+        $user = 'unknown';
+        if (isset($this->request->params['id'])) {
+            $user = $this->request->params['id'];
+        }
+        $this->response->body('<h2>executed Users.edit</h2><h2>edit: ' . $user . '</h2>');
+    }
     
 }
